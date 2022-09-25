a7bfe2f1fc98fb520a7fa2a9b40109e6f1618f88
unknown <mohamedhelalilyes@gmail.com>
2022-09-25 11:28:41 +0100

    Created the Register/Login controllers with the jwt generated

--- Backend/controllers/auth.controllers.js
+++ Backend/controllers/auth.controllers.js
@@ -1,14 +1,37 @@
 const userModel = require("../models/user.models");
+const errorHandClass = require("../utils/errorHandClass.utils");
+const bcrypt = require("bcrypt");
+require("dotenv").config({ path: "../config/config.env" });
 
 // @desc Register a new user
 // @route POST /api/v1/auth/register
 // @access Public
 module.exports.register = async (req, res, next) => {
   try {
+    const infos = req.body;
+
+    const existUser = await userModel.findOne({ email: infos.email });
+
+    if (existUser) {
+      return next(new errorHandClass("Email already exists", 422));
+    }
+
+    const salt = await bcrypt.genSalt(parseInt(process.env.SALT_ROUNDS));
+    const hashedPass = await bcrypt.hash(infos.password, salt);
+
+    const newUser = new userModel({
+      name: infos.name,
+      email: infos.email,
+      password: hashedPass,
+      role: infos.role,
+    });
+
+    await newUser.save();
+
     return res.status(200).json({
       success: true,
       error: null,
-      data: null,
+      data: "User Created",
     });
   } catch (err) {
     next(err);
@@ -20,10 +43,35 @@ module.exports.register = async (req, res, next) => {
 // @access Public
 module.exports.login = async (req, res, next) => {
   try {
+    const infos = req.body;
+
+    const existUser = await userModel
+      .findOne({
+        email: infos.email,
+      })
+      .select("+password");
+
+    if (!existUser) {
+      return next(new errorHandClass("Wrong Email/Password", 400));
+    }
+
+    const correctPass = await bcrypt.compare(
+      infos.password,
+      existUser.password
+    );
+
+    if (!correctPass) {
+      return next(new errorHandClass("Wrong Email/Password", 400));
+    }
+
+    await existUser.updateOne({ lastLogin: Date.now() });
+
+    const token = existUser.signToken();
+
     return res.status(200).json({
       success: true,
       error: null,
-      data: null,
+      token,
     });
   } catch (err) {
     next(err);
--- Backend/models/user.models.js
+++ Backend/models/user.models.js
@@ -1,51 +1,71 @@
 const mongoose = require("mongoose");
-
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please add a name"],
-  },
-  email: {
-    type: String,
-    required: [true, "Please add an email"],
-    unique: true,
-    match: [
-      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-      "Please add a valid email",
-    ],
-  },
-  role: {
-    type: String,
-    enum: ["user", "publisher", "admin"],
-    default: "user",
-  },
-  password: {
-    type: String,
-    required: [true, "Please add a password"],
-    minlength: 6,
-    select: false,
-  },
-  resetPasswordToken: String,
-  resetPasswordExpire: Date,
-  confirmEmailToken: String,
-  isEmailConfirmed: {
-    type: Boolean,
-    default: false,
-  },
-  twoFactorCode: String,
-  twoFactorCodeExpire: Date,
-  twoFactorEnable: {
-    type: Boolean,
-    default: false,
-  },
-  lastLogin: {
-    type: Date,
-    default: Date.now,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+const jwt = require("jsonwebtoken");
+require("dotenv").config({
+  path: "../config/config.env",
 });
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add a name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please add an email"],
+      unique: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please add a valid email",
+      ],
+    },
+    role: {
+      type: String,
+      enum: ["user", "publisher", "admin"],
+      default: "user",
+    },
+    password: {
+      type: String,
+      required: [true, "Please add a password"],
+      minlength: 6,
+      select: false,
+    },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
+    confirmEmailToken: String,
+    isEmailConfirmed: {
+      type: Boolean,
+      default: false,
+    },
+    twoFactorCode: String,
+    twoFactorCodeExpire: Date,
+    twoFactorEnable: {
+      type: Boolean,
+      default: false,
+    },
+    lastLogin: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.methods.signToken = function () {
+  return jwt.sign(
+    {
+      id: this._id,
+      name: this.name,
+      email: this.email,
+      role: this.role,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "10h",
+    }
+  );
+};
 
 module.exports = mongoose.model("User", userSchema);
